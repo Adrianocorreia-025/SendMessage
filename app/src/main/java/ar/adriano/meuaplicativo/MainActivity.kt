1122f02e9485b364f158a3790ff3ae8f4b71a5d2
Adrianocorreia-025 <82658411+Adrianocorreia-025@users.noreply.github.com>
2021-04-21 21:33:20 +0100

    Aplicacao para enviar sms

---
+++ app/src/main/java/ar/adriano/meuaplicativo/MainActivity.kt
@@ -0,0 +1,29 @@
+package ar.adriano.meuaplicativo
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    @SuppressLint("WrongViewCast")
+    fun sendMensage(view: View) {
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
+        val message = editTextMensagem.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+        putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+}
